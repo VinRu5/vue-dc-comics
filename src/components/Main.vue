c12989e5ff31a616097b72041e598d5017596e71
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-07-12 17:08:43 +0200

    Navbar e Header

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,8 @@
 <template>
     <main>
+        <div class="container">
         -->Content goes here
+        </div>
     </main>
 </template>
 
@@ -11,5 +13,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import '../style/colors.scss';
 
+    main {
+        background: $color-black;
+        color: $color-white;
+        font-size: 24px;
+        padding: 50px 0;
+        font-weight: bold;
+    }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,26 @@
 <template>
-    <nav>
-        navbar
-    </nav>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../assets/dc-logo.png" alt="logo dc comics">
+            </div>
+
+            <nav>
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 </template>
 
 <script>
@@ -11,5 +30,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import '../style/colors.scss';
 
+    header .container {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+
+        nav {
+
+            ul {
+                display: flex;
+
+                li {
+                    font-size: 14px;
+                    font-weight: 600;
+                    margin-left: 18px;
+                    height: 140px;
+                    line-height: 140px;
+        
+                    a {
+                        text-decoration: none;
+                        color: $color-ligth-black;
+
+        
+                        &:hover {
+                            border-bottom: 6px solid $color-blue; //da sistemare
+                            color: $color-blue;
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
