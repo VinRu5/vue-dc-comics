6d8721f1a24a5e08a882984d38e56d0e72213833
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-07-12 17:50:27 +0200

    seconda parte footer

--- src/components/FooterBottombar.vue
+++ src/components/FooterBottombar.vue
@@ -1,7 +1,29 @@
 <template>
-    <footer>
-        bottombar
-    </footer>
+    <section class="footer-bottombar">
+        <div class="container">
+            <div class="sign-up">
+                SIGN-UP NOW!
+            </div>
+            <div class="follow-us">
+                <div class="follow-text">FOLLOW US</div>
+                <div class="follow-item">
+                    <img src="../assets/footer-facebook.png" alt="facebook">
+                </div>
+                <div class="follow-item">
+                    <img src="../assets/footer-twitter.png" alt="twitter">
+                </div>
+                <div class="follow-item">
+                    <img src="../assets/footer-youtube.png" alt="youtube">
+                </div>
+                <div class="follow-item">
+                    <img src="../assets/footer-pinterest.png" alt="pinterest">
+                </div>
+                <div class="follow-item">
+                    <img src="../assets/footer-periscope.png" alt="periscope">
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -11,5 +33,42 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @import '../style/colors.scss';
+
+    .footer-bottombar {
+        background-color: $color-ligth-black;
+
+        .container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 48px 0;
+
+            .sign-up {
+                color: $color-white;
+                font-size: 20px;
+                font-weight: 500;
+                border: 2px solid $color-blue;
+                padding: 12px;
+
+            }
+
+            .follow-us {
+                display: flex;
+                align-items: center;
+
+                .follow-text {
+                    font-size: 26px;
+                    color: $color-blue;
+                    font-weight: bold;
+                }
+
+                .follow-item {
+                    margin-left: 12px;
+                }
+            }
+        }
+
+    }
 
 </style>
