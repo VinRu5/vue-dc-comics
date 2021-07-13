8fe2869b9fa6e5f103dc757080a5c9ec899e85d4
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-07-13 18:23:56 +0200

    caricamento dati liste

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,213 @@ export default {
   data: function() {
     return {
       comics: comicsJson,
+      navList: [
+        {
+          id: 0,
+          name: 'CHARACTERS',
+          href: '#'
+        },
+        {
+          id: 1,
+          name: 'COMICS',
+          href: '#'
+        },
+        {
+          id: 2,
+          name: 'MOVIES',
+          href: '#'
+        },
+        {
+          id: 3,
+          name: 'TV',
+          href: '#'
+        },
+        {
+          id: 4,
+          name: 'GAMES',
+          href: '#'
+        },
+        {
+          id: 5,
+          name: 'COLLECTIBLES',
+          href: '#'
+        },
+        {
+          id: 6,
+          name: 'VIDEOS',
+          href: '#'
+        },
+        {
+          id: 7,
+          name: 'FANS',
+          href: '#'
+        },
+        {
+          id: 8,
+          name: 'NEWS',
+          href: '#'
+        },
+        {
+          id: 9,
+          name: 'shop',
+          href: '#'
+        },
+      ],
+      footerList: [
+        [
+          {
+            id: 0,
+            name: 'DC COMICS',
+            href: ''
+          },
+          {
+            id: 1,
+            name: 'Characters',
+            href: '#'
+          },
+          {
+            id: 2,
+            name: 'Comics',
+            href: '#'
+          },
+          {
+            id: 3,
+            name: 'Movies',
+            href: '#'
+          },
+          {
+            id: 4,
+            name: 'TV',
+            href: '#'
+          },
+          {
+            id: 5,
+            name: 'Games',
+            href: '#'
+          },
+          {
+            id: 6,
+            name: 'Videos',
+            href: '#'
+          },
+          {
+            id: 7,
+            name: 'News',
+            href: '#'
+          },
+        ],
+        [
+          {
+            id: 0,
+            name: 'SHOP',
+            href: ''
+          },
+          {
+            id: 1,
+            name: 'Shop DC',
+            href: '#'
+          },
+          {
+            id: 2,
+            name: 'Shop DC Collectibles',
+            href: '#'
+          },
+        ],
+        [
+          {
+            id: 0,
+            name: 'DC',
+            href: ''
+          },
+          {
+            id: 1,
+            name: 'Term Of Use',
+            href: '#'
+          },
+          {
+            id: 2,
+            name: 'Privacy policy(New)',
+            href: '#'
+          },
+          {
+            id: 3,
+            name: 'Ad Choices',
+            href: '#'
+          },
+          {
+            id: 4,
+            name: 'Advertising',
+            href: '#'
+          },
+          {
+            id: 5,
+            name: 'Jobs',
+            href: '#'
+          },
+          {
+            id: 6,
+            name: 'Subscriptions',
+            href: '#'
+          },
+          {
+            id: 7,
+            name: 'Talent Workshops',
+            href: '#'
+          },
+          {
+            id: 8,
+            name: 'CPSC Certificates',
+            href: '#'
+          },
+          {
+            id: 9,
+            name: 'Ratings',
+            href: '#'
+          },
+          {
+            id: 10,
+            name: 'Shop Help',
+            href: '#'
+          },
+          {
+            id: 11,
+            name: 'Concatc Us',
+            href: '#'
+          },
+        ],
+        [
+          {
+            id: 0,
+            name: 'SITES',
+            href: '#'
+          },
+          {
+            id: 1,
+            name: 'DC',
+            href: '#'
+          },
+          {
+            id: 2,
+            name: 'MAD Magazine',
+            href: '#'
+          },
+          {
+            id: 3,
+            name: 'DC Kids',
+            href: '#'
+          },
+          {
+            id: 4,
+            name: 'DC Universe',
+            href: '#'
+          },
+          {
+            id: 5,
+            name: 'DC Power Visa',
+            href: '#'
+          },
+        ]
+      ]
     }
   }
 }
